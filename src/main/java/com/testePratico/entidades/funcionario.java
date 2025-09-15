6cd82bf64bd375683060d9cc85fa85d86fe5f335
Vitorio Suriano <vitoriosuriano.01@gmail.com>
2025-09-15 14:44:55 -0300

    Inicio do Projeto: Criando Entidades

---
+++ src/main/java/com/testePratico/entidades/funcionario.java
@@ -0,0 +1,8 @@
+package com.testePratico.entidades;
+
+import java.math.BigDecimal;
+
+public class funcionario extends pessoa{
+    private BigDecimal salario;
+    private String função;
+}
---
+++ src/main/java/com/testePratico/entidades/pessoa.java
@@ -0,0 +1,24 @@
+package com.testePratico.entidades;
+
+import java.time.LocalDate;
+
+public class pessoa {
+    private String nome;
+    private LocalDate dataNascimento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
